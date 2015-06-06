b50497377bfd9bef1025a379f96d40eaf582c86e
Jsong <songfengjv@sina.cn>
2015-06-06 16:24:19 +0800

    Util工具类

--- src/util/Util.java
+++ src/util/Util.java
@@ -1,12 +1,18 @@
 package util;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 
 /**
  * @author Jsong
@@ -71,6 +77,119 @@ public class Util {
 			if (out != null) {
 				out.close();
 			}
+		}
+	}
+	
+	/**
+	 * 将List集合存入指定的文件中
+	 * 
+	 * @param file	指定的文件
+	 * @param list	要存入的List集合
+	 */
+	public static void saveList(File file , List list){
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(file);
+			for (Object obj: list) {
+				out.print(obj);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+	
+	/**
+	 * 在字节数组ary中读取一个int数据
+	 * 
+	 * @param ary		字节数组
+	 * @param offest	偏移量
+	 * @return
+	 */
+	public static int toInt(byte[] ary,int offest){
+		DataInputStream in = null;
+		try {
+			ByteArrayInputStream bins = new ByteArrayInputStream(ary);
+			in = new DataInputStream(bins);
+			in.skip(offest);
+			return in.readInt();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 在字节数组ary中读取一个short数据
+	 * 
+	 * @param ary		字节数组
+	 * @param offest	偏移量
+	 * @return
+	 */
+	public static short toShort(byte[] ary,int offest){
+		DataInputStream in = null;
+		try {
+			ByteArrayInputStream bins = new ByteArrayInputStream(ary);
+			in = new DataInputStream(bins);
+			in.skip(offest);
+			return in.readShort();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 在字节数组ary中读取一个String数据
+	 * 
+	 * @param ary		字节数组
+	 * @param offest	偏移量
+	 * @param length 	长度
+	 * @return
+	 */
+	public static String toString(byte[] ary,int offest,int length){
+		try {
+			String str = new String(ary,offest,length,"UTF-8");
+			return str.trim();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * 休眠指定时间
+	 * 
+	 * @param millis	休眠的时间（毫秒）
+	 */
+	public static void sleep(long millis){
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
 
